b1d8883a65ff334b84654745a52bfdf7a942c290
Raynebul <rayman0678@gmail.com>
2025-09-08 23:13:57 +0700

    start page style

--- frontend/src/app/components/StartPage.component.ts
+++ frontend/src/app/components/StartPage.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { ImportsModule } from '../imports';
 
 @Component({
@@ -6,12 +6,25 @@ import { ImportsModule } from '../imports';
   standalone: true,
   imports: [ImportsModule],
   templateUrl: '../templates/StartPage.component.html',
+  styleUrl: '../styles/StartPage.component.scss'
 })
+
 export class StartPageComponent {
   title = 'frontend';
   events: string[];
 
   constructor() {
-    this.events = ['2020', '2021', '2022', '2023'];
+    this.events = ['2000', '2013', '2025'];
   }
+
+  x = 0;
+  y = 0;
+
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    const target = (event.target as HTMLElement).getBoundingClientRect();
+    this.x = event.clientX - target.left;
+    this.y = event.clientY - target.top;
+  }
+
 }
